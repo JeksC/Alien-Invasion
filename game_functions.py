dce6dacd5584c447b101c49c6c85a062fcd6ffa7
Jax Choong <choongjax@gmail.com>
2023-10-17 23:14:37 +0800

    Added ship movement

--- game_functions.py
+++ game_functions.py
@@ -2,11 +2,37 @@ import sys
 
 import pygame
 
-def check_events():
+def check_keydown_events(event,ship):
+    """Respond to keypresses"""
+    #Checks for user keyboard input
+    if event.key == pygame.K_RIGHT:
+        # Move ship to right
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+            ship.moving_left = True
+
+def check_keyup_events(event,ship):
+    """Respond to key releases"""
+    # Checks when user stops keyboard input
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+            ship.moving_left = False
+
+def check_events(ship):
     """Respond to keypresses and mouse events. """
     for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT:\
+                    # Checks if user quits the game
                     sys.exit()
+                
+                elif event.type == pygame.KEYDOWN:
+                    check_keydown_events(event,ship)
+                
+                elif event.type == pygame.KEYUP:
+                    check_keyup_events(event,ship)
+
+                        
 
 def update_screen(ai_settings,screen,ship):
     """Updates images on the screen and flips to new scren."""
