e2bceb7fa45bb07bdcc2d3d7c26d5a0043d9c3d4
Jax Choong <choongjax@gmail.com>
2023-11-20 11:17:20 +0800

    Added "Play" button

--- game_stats.py
+++ game_stats.py
@@ -8,6 +8,9 @@ class GameStats():
         # Start Alien Invasion in an active state.
         self.game_active = True
 
+        # Start game in an inactive state.
+        self.game_active = False
+
     def reset_stats(self):
         """Initialise statistics that can change during the game."""
         self.ships_left = self.ai_settings.ship_limit
